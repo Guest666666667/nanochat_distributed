8600e66a72398bfdf4ef243df29c5a5e10c26fb4
guest67 <1650202750@qq.com>
2025-11-10 21:43:00 -0330

    fix deepspeed

--- cluster_speedrun_mul_DP.sh
+++ cluster_speedrun_mul_DP.sh
@@ -21,8 +21,6 @@ export MASTER_ADDR=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
 export MASTER_PORT=29500
 export NUM_NODES=2
 export NUM_GPUS=4
-#export PYTHONPATH="/home/user/Desktop/nanochat_distributed/.venv/lib/python3.10/site-packages:$PYTHONPATH"
-
 
 echo "Host="$(hostname)
 echo "NODELIST="${SLURM_NODELIST}
