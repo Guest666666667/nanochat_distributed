381a1c0f69e3c966f180b3a5f461f552689fcbe3
guest67 <1650202750@qq.com>
2025-11-17 12:10:08 -0330

    deepspeed+tp seems good now

--- scripts/base_train_TP.py
+++ scripts/base_train_TP.py
@@ -196,13 +196,12 @@ param_groups = [
     }
 ]
 
-print(f"Initialize deepspeed in rank: {ddp_rank}")
 model_engine, optimizer, _, _ = deepspeed.initialize(
     model=model,
     model_parameters=param_groups,
     config=deepspeed_config
 )
-print(f"Initialized! {ddp_rank}")
+print(f"Initialized deepspeed in rank: {ddp_rank}")
 
 # Initialize the DataLoaders for train/val
 base_dir = get_base_dir()
